35ddd6f9546188d4d7564f8ea1b4959cdac25fb0
0 <gl@neb.best>
2023-12-24 08:06:31 -0500

    no clue

---
+++ Engine.cc
@@ -0,0 +1,8 @@
+//
+// Created by gl on 12/23/23.
+//
+
+#include "Engine.hh"
+
+namespace yaga {
+} // yaga
---
+++ Engine.hh
@@ -0,0 +1,16 @@
+//
+// Created by gl on 12/23/23.
+//
+
+#ifndef YAGE_ENGINE_HH
+#define YAGE_ENGINE_HH
+
+namespace yaga {
+
+    class Engine {
+
+    };
+
+} // yaga
+
+#endif //YAGE_ENGINE_HH
